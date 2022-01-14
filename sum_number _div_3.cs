4ecbfac76d4daafa2d9e93067e0ce1a941180ef3
anv <88430353+anvgithub@users.noreply.github.com>
2022-01-14 18:43:01 +0200

    Create sum_number _div_3.cs

---
+++ sum_number _div_3.cs
@@ -0,0 +1,15 @@
+        
+        int sum = 0;
+        for (int i = 1; i <= 20 ; i++)
+        {
+            if (i % 3 == 0) 
+            
+            {
+            sum = i + sum;
+             
+            }
+        
+        }
+        Console.WriteLine(sum);
+
+    
